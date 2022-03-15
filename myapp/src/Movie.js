038db931f28f0845f6363f81f3866d4fe13e23ee
V-Piraisoodan <piraisoodanv1996@gmail.com>
2022-03-16 00:33:58 +0530

    useHistory added

--- myapp/src/Movie.js
+++ myapp/src/Movie.js
@@ -1,28 +1,54 @@
 import { Counter } from './Counter.js';
 import { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import InfoIcon from '@mui/icons-material/Info';
+import {useHistory} from 'react-router-dom';
 
-export function Movie({ pic, moviename, rating, para }) {
+export function Movie({ pic, moviename, rating, para ,index}) {
 
   // hide para
   const [show , setShow] = useState(true);
+  const history = useHistory();
 
   // conditional styling
   // const styles = {display : show ? "block" : "none"}
 
-  return (
-    <div className="movie-container">
+return (
+    // <div className="movie-container">
+  <Card className="movie-container">
       <img className="movie-poster"
         src={pic}
         alt={moviename} />
+    {/* <CardContent>  vendum yendral use pannikkollalam  *************** */}
       <div className="movie-specs">
-        <h3 className="movie-name">{moviename}</h3>
+        <h3 className="movie-name">
+          {moviename}
+          <IconButton onClick={()=>setShow(!show)} color="primary" style={{marginBottom:"10px"}}>
+            {show ? <ExpandLessIcon /> : <ExpandMoreIcon/>}
+          </IconButton>
+          <IconButton  onClick={()=> history.push("/movies/" + index)} color="primary" style={{marginBottom:"10px"}}> {/*  interpolation method --> history.push(`/movies/$(index)`)  */}
+            <InfoIcon/>
+          </IconButton>
+          </h3>
         <p className="movie-rating">⭐{rating}</p>
       </div>
 
       {/* hide the movie paragraph */}
       {/* <button onClick={()=>setShow(!show)} style={{marginBottom:"10px"}} > {show ? "Hide" : "Show"} description</button> */}
-      <Button variant="outlined" onClick={()=>setShow(!show)} style={{marginBottom:"10px"}} >{show ? "Hide" : "Show"} description</Button>
+
+      {/* material ui */}
+      {/* <Button variant="outlined" onClick={()=>setShow(!show)} style={{marginBottom:"10px"}} >{show ? "Hide" : "Show"} description</Button> */}
+
+      {/* material ui   H3 KU ADD PANNANUM  */} 
+      {/* <IconButton onClick={()=>setShow(!show)} style={{marginBottom:"10px"}}>
+      {show ? <ExpandLessIcon /> : <ExpandMoreIcon/>}
+      </IconButton> */}
+
        
        {/* conditional styling */}
       {/* <p style={styles} className="movie-summary">{para}</p> */}
@@ -32,6 +58,8 @@ export function Movie({ pic, moviename, rating, para }) {
 
       {/* Nested component - one component is using with inside the another component*/}
       <Counter />
-    </div>
+    {/* </CardContent> */}
+    {/* </div> */}
+  </Card>
   );
 } 
