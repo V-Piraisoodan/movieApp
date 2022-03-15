90da3e45e8e01d7b442cc399d071a49123456590
V-Piraisoodan <piraisoodanv1996@gmail.com>
2022-03-16 00:34:20 +0530

    trailer probs added

--- myapp/src/Movielist.js
+++ myapp/src/Movielist.js
@@ -4,8 +4,8 @@ import { Movie } from './Movie';
 export function Movielist({ movie }) {
   return (
     <section className="movie-list">
-      {movie.map(({ moviename, pic, rating, para }) => (
-        <Movie moviename={moviename} pic={pic} rating={rating} para={para} />
+      {movie.map(({ moviename, pic, rating, para, trailer },index) => (
+        <Movie key={index} moviename={moviename} pic={pic} rating={rating} para={para} trailer={trailer} index={index}/>
       ))}
     </section>
   ); 
