36df5045e16a1b7dbc11bcc3953fd34783c5a84d
V-Piraisoodan <piraisoodanv1996@gmail.com>
2022-07-16 23:57:07 +0530

    button added

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import {Movielist} from './movies/Movielist.js'
-import Button from '@mui/material/Button';
 import { useState } from 'react';
 import {Switch,Link,Route,Redirect} from 'react-router-dom';
 import { Addcolour } from './color/Addcolour';
 import { initial_movielist } from './movies/initial_movielist';
-import TextField from '@mui/material/TextField';
 import { NotFound } from './NotFound';
 import { MoviesDetails } from './movies/MoviesDetails';
-import { useHistory } from 'react-router-dom';
+import { AddMovie } from './AddMovie';
 // import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
@@ -54,7 +52,7 @@ function App() {
         <Route path='/movies'>
           
 
-          <Movielist movie={movie}/>
+          <Movielist movie={movie} setmovie={setmovie}/>
 
         </Route>
 
@@ -79,50 +77,3 @@ function App() {
 
 export default App;
 
-function AddMovie({movie,setmovie}){
-  const [moviename, setMoviename] = useState("");
-  const [pic, setPic] = useState("");
-  const [para, setPara] = useState("");
-  const [rating, setRating] = useState("");
-
-  const history = useHistory();
-
-  const resetMovieForm = ()=>{
-      setMoviename("")
-      setPic("")
-      setPara("")
-      setRating("")
-  };
-
-  const Add_movie = ()=>{
-    console.log({moviename:moviename,pic:pic,para:para,rating:rating})
-    const newMovie = {                                          // shorthand
-      moviename,
-      pic,
-      para,
-      rating,
-    }
-    setmovie([ ...movie, newMovie ])
-    resetMovieForm()
-    history.push('/movies')
-  };
-  return(
-  <div>
-    <div className='input_fields'> 
-      <TextField id="outlined-basic" color="primary" size='small' label="Enter a movie name" variant="outlined" value={moviename} onChange={(event)=>setMoviename(event.target.value)} placeholder="Enter a movie name" />
-      <TextField id="outlined-basic" color="primary" size='small' label="Enter a poster URL" variant="outlined" value={pic} onChange={(event)=>setPic(event.target.value)} placeholder="Enter a poster URL" />
-      <TextField id="outlined-basic" color="primary" size='small' label="Enter a movie summary" variant="outlined" value={para} onChange={(event)=>setPara(event.target.value)} placeholder="Enter a movie summary" />
-      <TextField id="outlined-basic" color="primary" size='small' label="Enter a movie rating" variant="outlined" value={rating} onChange={(event)=>setRating(event.target.value)} placeholder="Enter a movie rating" />
-
-           {/* <input value={moviename} onChange={(event)=>setMoviename(event.target.value)} placeholder="Enter a movie name"/> */}
-           {/* <input value={pic} onChange={(event)=>setPic(event.target.value)} placeholder="Enter a poster URL"/> */}
-           {/* <input value={para} onChange={(event)=>setPara(event.target.value)} placeholder="Enter a movie summary"/> */}
-           {/* <input value={rating} onChange={(event)=>setRating(event.target.value)} placeholder="Enter a movie rating"/> */}
-    </div>
-    <div className='button'>
-      <Button className='button_' variant="contained" onClick={Add_movie} > Add movie </Button>
-    </div> 
-
- </div>
-  )
-}
