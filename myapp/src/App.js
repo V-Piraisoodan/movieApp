e59167c8c6647e60203c2fd15c434641d429470b
V-Piraisoodan <piraisoodanv1996@gmail.com>
2022-07-24 18:05:12 +0530

    adding dark and light theme and button

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -16,14 +16,35 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material/styles';  // dark theme import
+import Paper from '@mui/material/Paper';  // PAPER Import
+
+import Brightness4Icon from '@mui/icons-material/Brightness4';  // light and dark icon import
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 function App() {
 
   const [movie,setmovie] = useState(initial_movielist)
   const history = useHistory();
 
-  return (
-    <div className="App">
+  const [mode,setmode] = useState("light")
+  console.log(mode)
+  const darkTheme = createTheme({   //STEP:1 dark theme CONTEXT Creating
+    palette: {
+      mode: mode,
+    },
+  });
+
+  const paperStyles = {
+    borderRadius : "0px",
+    minHeight : "100vh"
+  }
+  
+
+  return (                          // STEP:2 Publishing
+  <ThemeProvider theme={darkTheme}> 
+    <Paper elevation={6} style={paperStyles}>
+      <div className="App">
  
       
         {/* <ul className='navication'>
@@ -33,78 +54,88 @@ function App() {
          <li><Link to='/color-game' className='navication_list'>Color-Game</Link></li>
         </ul> */}
 
-      <AppBar position="static">
-        <Toolbar>
-          <Button onClick={()=>history.push('/')}
-            size="large" color="inherit" aria-label="Home">
-            Home
-          </Button>
+        <AppBar position="static">
+          <Toolbar>
+            <Button onClick={()=>history.push('/')}
+              size="large" color="inherit" aria-label="Home">
+              Home
+            </Button>
 
-          <Button onClick={()=>history.push('/movies')}
-            size="large" color="inherit" aria-label="Movies">
-            Movies
-          </Button>
+            <Button onClick={()=>history.push('/movies')}
+              size="large" color="inherit" aria-label="Movies">
+              Movies
+            </Button>
 
-          <Button onClick={()=>history.push('/movies/add')}
-            size="large" color="inherit" aria-label="Add Movies">
-            Add Movies
-          </Button>
+            <Button onClick={()=>history.push('/movies/add')}
+              size="large" color="inherit" aria-label="Add Movies">
+              Add Movies
+            </Button>
 
-          <Button onClick={()=>history.push('/color-game')}
-            size="large" color="inherit" aria-label="Color-Game">
-            Color-Game
-          </Button>
-        </Toolbar>
-      </AppBar>
+            <Button onClick={()=>history.push('/color-game')}
+              size="large" color="inherit" aria-label="Color-Game">
+              Color-Game
+            </Button>
+
+            <Button onClick={()=>  setmode(mode === "dark" ? "light" : "dark")}
+              style = {{marginLeft:"auto"}}
+              size="large" color="inherit" aria-label="Mode"
+              endIcon={mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}>
+              {mode === "dark" ? "light" : "dark"} Mode
+              {/* {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />} */}
+            </Button>
+          </Toolbar>
+        </AppBar>
       
 
-      <Switch>
-        {/* home path */}
-        <Route exact path='/'>
-          <p>Welcome to the Movie App!!!!!</p>
-        </Route>
+        <Switch>
+          {/* home path */}
+          <Route exact path='/'>
+            <p>Welcome to the Movie App!!!!!</p>
+          </Route>
 
-        {/* film new path,it is redirected to the "/movies" path */}
-        <Route path='/films'>
-          <Redirect to="/movies"/>
-        </Route> 
+          {/* film new path,it is redirected to the "/movies" path */}
+          <Route path='/films'>
+            <Redirect to="/movies"/>
+          </Route> 
 
-        {/* add movies */}
-        <Route path='/movies/add'>
-          <AddMovie movie={movie} setmovie={setmovie}/>
-        </Route>
+          {/* add movies */}
+          <Route path='/movies/add'>
+            <AddMovie movie={movie} setmovie={setmovie}/>
+          </Route>
 
-        {/* edit movie */}
-        <Route path='/movies/edit/:id'>
-          <EditMovie movie={movie} setmovie={setmovie}/>
-        </Route>  
+          {/* edit movie */}
+          <Route path='/movies/edit/:id'>
+            <EditMovie movie={movie} setmovie={setmovie}/>
+          </Route>  
 
-        {/* dynamic route */}
-        <Route path='/movies/:id'>
-          <MoviesDetails movies={movie}/>
-        </Route>  
+          {/* dynamic route */}
+          <Route path='/movies/:id'>
+            <MoviesDetails movies={movie}/>
+          </Route>  
 
-        {/*movies path  */}
-        <Route path='/movies'>
-          <Movielist movie={movie} setmovie={setmovie}/>
-        </Route>
+          {/*movies path  */}
+          <Route path='/movies'>
+            <Movielist movie={movie} setmovie={setmovie}/>
+          </Route>
 
-        {/* color game path */}
-        <Route path='/color-game'>
-          <Addcolour />
-        </Route>
+          {/* color game path */}
+          <Route path='/color-game'>
+            <Addcolour />
+          </Route>
         
-        {/* 404 page path */}
-        <Route path="**">
-          <NotFound/>
-        </Route> 
-      </Switch>
+          {/* 404 page path */}
+          <Route path="**">
+            <NotFound/>
+          </Route> 
+        </Switch>
 
-      {/* <Counter/> */}
-      {/* <ColourBox/> */}
-      {/* <Addcolour /> */}
+        {/* <Counter/> */}
+        {/* <ColourBox/> */}
+        {/* <Addcolour /> */}
 
-    </div>
+      </div>
+    </Paper>
+  </ThemeProvider>
   );
 }
 
