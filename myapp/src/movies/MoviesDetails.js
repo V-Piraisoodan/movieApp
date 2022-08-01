2cf40cef61c38523977b6fc899eef0aea6a62812
V-Piraisoodan <piraisoodanv1996@gmail.com>
2022-08-01 16:27:56 +0530

    api get method included

--- myapp/src/movies/MoviesDetails.js
+++ myapp/src/movies/MoviesDetails.js
@@ -2,12 +2,24 @@ import Button from '@mui/material/Button';
 import { useParams } from 'react-router-dom';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useEffect , useState } from 'react';
 
-export function MoviesDetails({ movies }) {
+
+export function MoviesDetails() {
   const { id } = useParams();
-  const movie = movies[id];
+  // const movie = movies[id];
   const history = useHistory();
   // console.log(movie)
+
+  const [movie , setMovie] = useState({});
+  useEffect(()=>{
+    fetch(`https://62dd3993ccdf9f7ec2c27434.mockapi.io/movies/${id}`,
+    {
+      method: "GET",
+    })                                                          //to get the api
+    .then(data=>data.json())                                   // to convert the date into json format
+    .then(ans=> setMovie(ans))                                //get the output in console
+  }, []);
   return (
     <div className='trailer-container'>
       <iframe width="200%"
