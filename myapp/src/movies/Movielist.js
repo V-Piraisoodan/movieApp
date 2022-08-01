456fd88adc1d769c80287f92da52b236e4218671
V-Piraisoodan <piraisoodanv1996@gmail.com>
2022-08-01 16:27:28 +0530

    api get method and delete method included

--- myapp/src/movies/Movielist.js
+++ myapp/src/movies/Movielist.js
@@ -3,13 +3,52 @@ import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { useHistory } from 'react-router-dom';
+import { useEffect , useState } from 'react';
 
 // COUNTER IMPORT  111111111
-export function Movielist({ movie , setmovie}) {
+export function Movielist() {
   const history = useHistory();
+  const [movie,setmovie] = useState([])
+
+  const getMovies = () => {
+    fetch("https://62dd3993ccdf9f7ec2c27434.mockapi.io/movies",
+    {
+      method : "GET",
+    }) //to get the api
+    .then(data=>data.json())                                   // to convert the date into json format
+    .then(ans=> setmovie(ans)) 
+  }
+
+  useEffect(getMovies, []);
+
+  //api method
+  const removeMovie = (id) => {
+
+    //after delete --> refresh the movie details
+    fetch(`https://62dd3993ccdf9f7ec2c27434.mockapi.io/movies/${id}`,
+    {
+      method : "Delete",
+    })
+    .then (()=>getMovies())
+  }
+
+
+  //probs method
+
+  // const removeMovie = (index) => {
+  //   console.log(index)
+  //   //create a copy of movie list & remove the element from it -- filter
+  //   const removeMovieIndex = index;
+  //   const remainingMovies = movie.filter((mv ,idx)=>  //we pass the 2 arguments(mv & idx) value and index(removeMoviesIndex)   
+  //     idx !== removeMovieIndex
+  //   );
+  //   console.log(remainingMovies,movie,removeMovieIndex)
+  //   setmovie(remainingMovies);
+  //   }
+
   return (
     <section className="movie-list">
-      {movie.map(({ moviename, pic, rating, para, trailer },index) => (
+      {movie.map(({ moviename, pic, rating, para, trailer, id },index) => (
         <Movie 
           key={index} 
           moviename={moviename} 
@@ -17,18 +56,9 @@ export function Movielist({ movie , setmovie}) {
           rating={rating} 
           para={para} 
           trailer={trailer} 
-          index={index}
+          id={id}
           deleteButton = {
-           <IconButton onClick={()=>{
-              console.log(index)
-              //create a copy of movie list & remove the element from it -- filter
-              const removeMovieIndex = index;
-              const remainingMovies = movie.filter((mv ,idx)=>  //we pass the 2 arguments(mv & idx) value and index(removeMoviesIndex)   
-                idx !== removeMovieIndex
-              );
-              console.log(remainingMovies,movie,removeMovieIndex)
-              setmovie(remainingMovies);
-              } }color="error" aria-label="delete" >
+           <IconButton onClick={()=>removeMovie(id)}color="error" aria-label="delete" >
              <DeleteIcon />
             </IconButton>}
           editButton = {
